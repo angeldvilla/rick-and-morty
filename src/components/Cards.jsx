f7ed4a6e7695dfbaf25084bc99dd412469001f50
angeldvilla <escarlata.2003@gmail.com>
2023-03-06 19:29:46 -0500

    New React Project

---
+++ src/components/Cards.jsx
@@ -0,0 +1,26 @@
+import Card from './Card';
+
+const Cards = (props) => {
+   const { characters } = props;
+   return (
+
+   <div>
+      {characters.map((person) => {
+         return(
+            
+            <Card 
+             name = {person.name}
+             species = {person.species}
+             gender = {person.species}
+             image = {person.image}
+             onClose = {props.onClose}
+            />  
+         )
+      })}
+   </div>
+   
+   )
+}
+
+
+export default Cards;
